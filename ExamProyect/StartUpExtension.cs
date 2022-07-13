843ada38cdc0a4354935b82191c0f2945741f608
Emilio Liriano <emiliolirianomaldonado@gmail.com>
2022-07-13 12:55:20 -0400

    Cleaning StartUp

---
+++ ExamProyect/StartUpExtension.cs
@@ -0,0 +1,103 @@
+﻿using Authentication;
+using AutoMapper;
+using Domain.Model;
+using ExamProyect.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Repository.Repository;
+using Services.IServices;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProyect
+{
+    public static class StartUpExtension
+    {
+        public static void AddSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExamProyect", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Token Security"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                {
+                new OpenApiSecurityScheme
+                {
+                Reference = new OpenApiReference
+                {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+                }
+                },
+                new string[] {}
+
+                }
+                });
+            });
+        }
+        public static void AddMapperConfig(this IServiceCollection services)
+        {
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.CreateMap<User, UserModel>().ReverseMap();
+                mc.CreateMap<User, EditUserModel>().ReverseMap();
+                mc.CreateMap<Product, ProductModel>().ReverseMap();
+                mc.CreateMap<Product, EditProductModel>().ReverseMap();
+            });
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+        }
+
+        public static void AddJWTConfig(this IServiceCollection services, IConfiguration Configuration)
+        {
+            string JWTKey = Configuration.GetValue<string>("Jwt:Key");
+            services.AddSingleton<IJWTServices>(new JWTServices(JWTKey));
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JWTKey)),
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    RequireExpirationTime = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromMinutes(10)
+
+                };
+            });
+        }
+        public static void AddRepositoryConfig(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<IUser, UserService>();
+            services.AddTransient<IProduct, ProductService>();
+        }
+    }
+}
--- ExamProyect/Startup.cs
+++ ExamProyect/Startup.cs
@@ -1,28 +1,12 @@
-using Authentication;
-using AutoMapper;
-using Domain.Model;
-using ExamProyect.Model;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using Repository;
-using Repository.Repository;
-using Services.IServices;
-using Services.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ExamProyect
 {
@@ -38,78 +22,14 @@ namespace ExamProyect
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string JWTKey = Configuration.GetValue<string>("Jwt:Key");
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExamProyect", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description = "Token Security"
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                {
-                new OpenApiSecurityScheme
-                {
-                Reference = new OpenApiReference
-                {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-                }
-                },
-                new string[] {}
-
-                }
-                });
-            });
-
             services.AddDbContext<ApplicationDbContext>(item => item.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-            services.AddTransient<IUser, UserService>();
-            services.AddTransient<IProduct, ProductService>();
-            var mapperConfig = new MapperConfiguration(mc =>
-            {
-                mc.CreateMap<User, UserModel>().ReverseMap();
-                mc.CreateMap<User, EditUserModel>().ReverseMap();
-                mc.CreateMap<Product, ProductModel>().ReverseMap();
-                mc.CreateMap<Product, EditProductModel>().ReverseMap();
-            });
-            IMapper mapper = mapperConfig.CreateMapper();
-            services.AddSingleton(mapper);
-            services.AddSingleton<IJWTServices>(new JWTServices(JWTKey));
-            
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(x=> 
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JWTKey)),
-                    ValidateAudience = true,
-                    ValidateIssuer = true,
-                    ValidIssuer = Configuration["Jwt:Issuer"],
-                    ValidAudience = Configuration["Jwt:Audience"],
-                    RequireExpirationTime = true,
-                    ValidateLifetime = true,
-                    ClockSkew = TimeSpan.FromMinutes(10)
-
-                };
-            });
+            services.AddControllers();
+            services.AddSwaggerConfig();
+            services.AddMapperConfig();
+            services.AddJWTConfig(Configuration);
+            services.AddRepositoryConfig();
             services.AddAuthorization();
-            
-
+   
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
